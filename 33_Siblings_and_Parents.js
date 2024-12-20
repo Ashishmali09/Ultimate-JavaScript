7bfb97ff942888d936eeb0097471977a82c4bd94
Ashish Mali <maliashish0912@gmail.com>
2024-12-20 20:07:15 +0530

    Cooments add for parentNode and ParentElement

--- 33_Siblings_and_Parents.js
+++ 33_Siblings_and_Parents.js
@@ -1,5 +1,5 @@
 console.log(document.body.firstChild);
 a = document.body.firstChild;
-console.log(a.parentNode); // It will be anything.
-console.log(a.parentElement); // It must be an element.
+console.log(a.parentNode);      //It will be anything.
+console.log(a.parentElement);   //It must be an element.
 console.log(a.firstChild.nextSibling);
