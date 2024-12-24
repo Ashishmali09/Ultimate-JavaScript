e0c964bc8909fa70b0945f68fc3b4a51342b1430
Ashish Mali <maliashish0912@gmail.com>
2024-12-24 18:51:12 +0530

    InnerHTML Property

--- 41_innerHTML.js
+++ 41_innerHTML.js
@@ -6,8 +6,8 @@ console.dir(y);
 // console.log(document.body.firstChild.nodeName);
 // console.log(document.body.firstElementChild.nodeName);
 
-// console.log(first.innerHTML);
-// console.log((first.innerHTML = "<i>Hey I am italic</i>"));
+console.log(first.innerHTML);
+console.log((first.innerHTML = "<i>Hey I am italic</i>"));
 
 // console.log(first.outerHTML);
 // console.log((first.outerHTML = "<Marquee>Hey I am Running</Marquee>"));
