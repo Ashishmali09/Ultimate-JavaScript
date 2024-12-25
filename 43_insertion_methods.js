7655c497bedb5c2c9ea9d399e70172daa6f16025
Ashish Mali <maliashish0912@gmail.com>
2024-12-25 19:08:00 +0530

    Add class into new div

--- 43_insertion_methods.js
+++ 43_insertion_methods.js
@@ -4,7 +4,7 @@ let a = document.getElementsByTagName("div")[0];
 
 let div = document.createElement("div");
 div.innerHTML = "<h1>Hello World!</h1>";
-// div.className = "Ash";
+div.className = "Ash";
 // a.append(div);
 // a.prepend(div);
 // a.before(div);
