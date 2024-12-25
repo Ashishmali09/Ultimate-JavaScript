18c5f33f7571a248e1367aa6e7af6d6a16f527a2
Ashish Mali <maliashish0912@gmail.com>
2024-12-25 22:42:47 +0530

    insertAdjacentMethods for HTML

--- 44_insertAdjacent.js
+++ 44_insertAdjacent.js
@@ -1,9 +1,9 @@
 let first = document.getElementById("first");
 
-// first.insertAdjacentHTML("beforebegin", "<div class='test'>before begin</div>");
-// first.insertAdjacentHTML("beforeend", "<div class='test'>before end</div>");
-// first.insertAdjacentHTML("afterbegin", "<div class='test'>after begin</div>");
-// first.insertAdjacentHTML("afterend", "<div class='test'>after end</div>");
+first.insertAdjacentHTML("beforebegin", "<div class='test'>before begin</div>");
+first.insertAdjacentHTML("beforeend", "<div class='test'>before end</div>");
+first.insertAdjacentHTML("afterbegin", "<div class='test'>after begin</div>");
+first.insertAdjacentHTML("afterend", "<div class='test'>after end</div>");
 
 let div = document.createElement("div");
 div.innerHTML = "<span>I am Span </span>";
@@ -18,4 +18,4 @@ let text = "We have to go to Japan";
 // first.insertAdjacentText("afterbegin", text);
 // first.insertAdjacentText("afterend", text);
 
-first.remove()
+// first.remove();
