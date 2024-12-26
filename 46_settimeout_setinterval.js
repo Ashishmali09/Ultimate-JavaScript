312831415ac073ff273e93ad037b4c1a568aaa5a
Ashish Mali <maliashish0912@gmail.com>
2024-12-26 18:41:24 +0530

    setTimeout and setInterval

--- 46_settimeout_setinterval.js
+++ 46_settimeout_setinterval.js
@@ -1,14 +1,14 @@
 document.write("Hello Dakota");
 
-// let a = setTimeout(function () {
-//   alert("Hey I am inside the settimeout");
-// }, 5000);
-// console.log(a);
+let a = setTimeout(function () {
+  alert("Hey I am inside the settimeout");
+}, 5000);
+console.log(a);
 
-// let b = prompt("Do you want to run this settimeout?");
-// if (b == "n") {
-//   clearTimeout(a);
-// }
+let b = prompt("Do you want to run this settimeout?");
+if (b == "n") {
+  clearTimeout(a);
+}
 
 // const sum = (a, b, z) => {
 //   console.log("Yes I am running a sum", a + b + z);
