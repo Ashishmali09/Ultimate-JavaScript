57fa72875ee007e49f55b12b04336677aed5fd6d
Ashish Mali <maliashish0912@gmail.com>
2025-01-04 21:39:27 +0530

    Promise .then multiple calls

--- 56_Promise_chaining.js
+++ 56_Promise_chaining.js
@@ -1,30 +1,30 @@
-// let promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     console.log("Resolved after 2 seconds");
-//     resolve(54);
-//   }, 2000);
-// });
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Resolved after 2 seconds");
+    resolve(54);
+  }, 2000);
+});
 
-// promise
-//   .then((value) => {
-//     console.log(value);
-//     let promise2 = new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve("promise 2");
-//       }, 2000);
-//     });
-//     return promise2; // When Promise2 fulfilled only then .then() will work and give its value
-//   })
-//   .then((value) => {
-//     console.log("We are done");
-//     return 9;
-//   })
-//   .then((value) => {
-//     console.log("We are pakka done");
-//   });
+promise
+  .then((value) => {
+    console.log(value);
+    let promise2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("promise 2");
+      }, 2000);
+    });
+    return promise2; // When Promise2 fulfilled only then .then() will work and give its value
+  })
+  .then((value) => {
+    console.log("We are done");
+    return 9;
+  })
+  .then((value) => {
+    console.log("We are pakka done");
+  });
 
 //Quiz to write loadScript function with promises
-const loadScript = (src) => {
+/*const loadScript = (src) => {
   return new Promise((resolve, reject) => {
     let script = document.createElement("script");
     script.type = "text/javascript";
@@ -54,4 +54,4 @@ p.then((value) => {
   .catch((error) => {
     console.log("We are sorry but we are having problems loading this script");
     // console.log(error);
-  });
+  });*/
