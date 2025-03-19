adb8b2b5d866764bc42c7c8e2b9d7cabe6b7ee79
Ashish Mali <maliashish0912@gmail.com>
2025-03-19 19:51:48 +0530

    Add dictionary api for application

--- 66_Fetch_API.js
+++ 66_Fetch_API.js
@@ -1,8 +1,10 @@
 let p = fetch("https://goweather.herokuapp.com/weather/Ny");
+// let p = fetch("https://api.dictionaryapi.dev/api/v2/entries/en/fart");
 p.then((response) => {
   console.log(response.status);
   console.log(response.ok);
   console.log(response.headers);
+  // console.log(response.text());
   return response.json();
 }).then((response) => {
   console.log(response);
